7f002631fd0b9ba1cc7cf8a30a0b5c63c66d1753
Sebastian Derendarz <Derendarz99@gmail.com>
2022-02-06 21:43:10 +0100

    T-32 Add Admin, Customer, Barber models

---
+++ api/auth/models.py
@@ -0,0 +1,79 @@
+import uuid
+
+from django.contrib import auth as auth_models
+from django.contrib.auth import password_validation
+from django.core import validators
+from django.db import models
+from django.utils.translation import gettext_lazy as __
+
+from api.auth import utils, value_objects
+
+
+class UserManager(auth_models.BaseUserManager):
+    '''Form for creating all types of users.'''
+
+    def create_admin(self, email: str, name: str, password: str, surname: str) -> 'User':
+        return self._create_user(account_type=value_objects.AccountType.ADMIN, email=email, name=name, password=password, surname=surname)
+
+    def create_barber(self, email: str, name: str, password: str, surname: str) -> 'User':
+        return self._create_user(account_type=value_objects.AccountType.BARBER, email=email, name=name, password=password, surname=surname)
+
+    def create_customer(self, email: str, name: str, password: str, surname: str) -> 'User':
+        return self._create_user(account_type=value_objects.AccountType.CUSTOMER, email=email, name=name, password=password, surname=surname)
+
+    def _create_user(self, account_type: value_objects.AccountType, email: str, name: str, password: str, surname: str) -> 'User':
+        user = self.model(
+            account_type=account_type,
+            email=self.normalize_email(email),
+            name=name,
+            surname=surname,
+        )
+        password_validation.validate_password(password)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(auth_models.AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(
+        __('Email'),
+        max_length=150,
+        unique=True,
+        help_text=__('Valid email address is required'),
+    )
+    name = models.CharField(
+        __('Name'),
+        max_length=100,
+        validators=[validators.MinLengthValidator(2)],
+        help_text=__('Valid name is required'),
+    )
+    surname = models.CharField(
+        __('Surname'),
+        max_length=150,
+        validators=[validators.MinLengthValidator(2)],
+        help_text=__('Valid surname is required'),
+    )
+    created_at = models.DateTimeField(__('Created at'), auto_now_add=True)
+    updated_at = models.DateTimeField(__('Updated at'), auto_now=True)
+    email_confirmation_token = models.UUIDField(default=uuid.uuid4)
+    email_confirmation_token_ttl = models.DateTimeField(default=utils.get_email_confirmation_token_expiration_time)
+    account_status = models.CharField(__('Account Status'), default=value_objects.AccountStatus.UNDER_VERIFICATION.name, **utils.enum_to_char_field_args(value_objects.AccountStatus))
+    account_type = models.CharField(__('Account Type'), **utils.enum_to_char_field_args(value_objects.AccountType))
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email', 'name', 'surname']
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        '''Human readable representation of object.'''
+
+        return f'{self.name} {self.surname}'
+
+    def delete(self) -> None:
+        '''Override model delete method to soft delete User.'''
+
+        if self.account_status not in (value_objects.AccountStatus.CLOSED.name, value_objects.AccountStatus.DELETED.name):
+            self.account_status = value_objects.AccountStatus.CLOSED.name
+            self.save()
--- api/auth/utils.py
+++ api/auth/utils.py
@@ -7,14 +7,14 @@ from api.core import utils
 
 
 def get_email_confirmation_token_expiration_time() -> timezone.datetime:
-    """Get email confirmation token expiration datetime from environment variable."""
+    '''Get email confirmation token expiration datetime from environment variable.'''
     env_var_name = 'EMAIL_CONFIRMATION_TOKEN_TTL'
     try:
         expiration_time = int(utils.get_env_value(env_var_name, '240'))
     except ValueError:
-        exceptions.ImproperlyConfigured(f"Set int value for {env_var_name} environment variable.")
+        exceptions.ImproperlyConfigured(f'Set int value for {env_var_name} environment variable.')
     if expiration_time <= 0:
-        exceptions.ImproperlyConfigured(f"Set positive int value for {env_var_name} environment variable.")
+        exceptions.ImproperlyConfigured(f'Set positive int value for {env_var_name} environment variable.')
     return timezone.now() + timezone.timedelta(minutes=expiration_time)
 
 

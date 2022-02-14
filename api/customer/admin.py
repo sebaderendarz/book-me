059f5bba10a9fa93ff07988617f93d14979de0b3
Sebastian Derendarz <Derendarz99@gmail.com>
2022-02-14 21:18:44 +0100

    T-43 Add user permissions to django admin

--- api/customer/admin.py
+++ api/customer/admin.py
@@ -1,25 +1,26 @@
 # type:ignore
 from django.contrib import admin
 
-from customer import models, utils, value_objects
+from authentication import value_objects as auth_value_objects
+from customer import models, utils, value_objects as customer_value_objects
 
 
 @admin.action(description='Renew selected service orders')
 def make_new(modeladmin, request, queryset):  # pylint: disable=unused-argument
     '''Change status of selected offers to NEW.'''
-    queryset.update(status=value_objects.OrderStatus.NEW.name)
+    queryset.update(status=customer_value_objects.OrderStatus.NEW.name)
 
 
 @admin.action(description='Close selected service orders')
 def make_closed(modeladmin, request, queryset):  # pylint: disable=unused-argument
     '''Change status of selected offers to CLOSED.'''
-    queryset.update(status=value_objects.OrderStatus.CLOSED.name)
+    queryset.update(status=customer_value_objects.OrderStatus.CLOSED.name)
 
 
 @admin.action(description='Confirm selected service orders')
 def make_confirmed(modeladmin, request, queryset):  # pylint: disable=unused-argument
     '''Change status of selected offers to HIDDEN.'''
-    queryset.update(status=value_objects.OrderStatus.CONFIRMED.name)
+    queryset.update(status=customer_value_objects.OrderStatus.CONFIRMED.name)
 
 
 @admin.register(models.ServiceOrder)
@@ -31,18 +32,19 @@ class ServiceOrderAdmin(admin.ModelAdmin):
         'customer',
         'offer',
     )
-    readonly_fields = (
-        'token',
-        'service_time',
-        'customer',
-        'offer',
-    )
     list_filter = (utils.OrderStatusFilter, utils.OrderServiceTimeFilter)
     actions = (make_new, make_confirmed, make_closed)
     search_fields = ('token',)
+    raw_id_fields = ('customer', 'offer')
 
-    def has_add_permission(self, request):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
+    def get_readonly_fields(self, request, obj=None):
+        is_user_admin = request.user.account_type == auth_value_objects.AccountType.ADMIN.name
+        only_admin_editable_fields = (
+            'token',
+            'service_time',
+            'customer',
+            'offer',
+        )
+        if obj and not is_user_admin:
+            return self.readonly_fields + only_admin_editable_fields
+        return self.readonly_fields

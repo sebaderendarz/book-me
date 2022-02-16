640075efc0cfeb85ade6fa016ddbc3f659e758e9
Sebastian Derendarz <Derendarz99@gmail.com>
2022-02-16 19:34:57 +0100

    T-43 Remove return type from celery task

--- api/tasks/image_handling/resize_image.py
+++ api/tasks/image_handling/resize_image.py
@@ -7,10 +7,8 @@ from tasks import celery
 
 
 @celery.app.task
-def resize_image_at_path(path: str, output_size: Tuple[int, int]) -> bool:
-    if not os.path.isfile(path):
-        return False
-    image = Image.open(path)
-    image.thumbnail(output_size)
-    image.save(path)
-    return True
+def resize_image_at_path(path: str, output_size: Tuple[int, int]) -> None:
+    if os.path.isfile(path):
+        image = Image.open(path)
+        image.thumbnail(output_size)
+        image.save(path)

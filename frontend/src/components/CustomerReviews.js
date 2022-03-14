ef0fdf66932cee6470d7da5f283b8d2bab36b580
Sebastian Derendarz <Derendarz99@gmail.com>
2022-03-14 09:44:05 +0100

    T-16,17,20,17 Add barber landing page v2

---
+++ frontend/src/components/CustomerReviews.js
@@ -0,0 +1,62 @@
+const review1 = `# Amazing experience
+
+_March 1, 2022 by [Olivier](/)_
+
+Duis sed ipsum pretium, vulputate lectus eu, tristique felis. Nam sit amet mollis felis. 
+Pellentesque in orci elit. Cras imperdiet diam nec **magna sollicitudin**, nec semper mi aliquet. 
+
+Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum.
+Aenean lacinia bibendum nulla sed consectetur.
+
+## What makes a difference
+
+Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+
+### **Design**
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+- Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+- Donec id elit non mi porta gravida at eget metus.
+
+Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
+
+1. Vestibulum id ligula porta felis euismod semper.
+1. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.
+`;
+
+const review2 = `# Simplicity is what makes this app offbeat
+
+_February 23, 2022 by [Matt](/)_
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Aenean eu leo quam. Pellentesque _ornare sem lacinia_ quam venenatis vestibulum.
+Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
+
+Curabitur blandit tempus porttitor. **Nullam quis risus eget urna mollis** ornare vel eu leo.
+Nullam id dolor id nibh ultricies vehicula ut id elit.
+`;
+
+const review3 = `# Keep it up, the newest features are great
+
+_February 14, 2022 by [Tom](/)_
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.
+Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
+ut fermentum massa justo sit amet risus.
+
+- Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+- Donec id elit non mi porta gravida at eget metus.
+- Nulla vitae elit libero, a pharetra augue.
+
+Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
+`;
+
+const customerReviews = [review1, review2, review3];
+
+export default customerReviews;

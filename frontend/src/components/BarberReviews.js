4f4f42202fca5180314ef25c7c5e8dde0c0de17d
Sebastian Derendarz <Derendarz99@gmail.com>
2022-03-14 09:43:53 +0100

    T-16,17,20,17 Add barber landing page

---
+++ frontend/src/components/BarberReviews.js
@@ -0,0 +1,62 @@
+const review1 = `# The best user experience in the industry
+
+_March 10, 2022 by [Olga](/)_
+
+Duis sed ipsum pretium, vulputate lectus eu, tristique felis. Nam sit amet mollis felis. 
+Pellentesque in orci elit. Cras imperdiet diam nec **magna sollicitudin**, nec semper mi aliquet. 
+
+Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum.
+Aenean lacinia bibendum nulla sed consectetur.
+
+## Key differences
+
+Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+
+### **Design**
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+- Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+- Donec id elit non mi porta gravida at eget metus.
+
+Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
+
+1. Vestibulum id ligula porta felis euismod semper.
+1. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.
+`;
+
+const review2 = `# Practical usage over imaginary design, that's what drives this app
+
+_February 28, 2022 by [John](/)_
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Aenean eu leo quam. Pellentesque _ornare sem lacinia_ quam venenatis vestibulum.
+Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
+
+Curabitur blandit tempus porttitor. **Nullam quis risus eget urna mollis** ornare vel eu leo.
+Nullam id dolor id nibh ultricies vehicula ut id elit.
+`;
+
+const review3 = `# Simplicity is what makes this app offbeat
+
+_February 20, 2022 by [Ron](/)_
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.
+Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
+ut fermentum massa justo sit amet risus.
+
+- Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+- Donec id elit non mi porta gravida at eget metus.
+- Nulla vitae elit libero, a pharetra augue.
+
+Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
+`;
+
+const barberReviews = [review1, review2, review3];
+
+export default barberReviews;

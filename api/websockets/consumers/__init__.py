411edb252aa0da39aae15c8f546405c26f165def
Sebastian Derendarz <Derendarz99@gmail.com>
2022-03-27 11:36:38 +0200

    T-27 Add ServiceUnavailabilitiesConsumer

---
+++ api/websockets/consumers/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['ServiceOfferConsumer', 'ServiceUnavailabilitiesConsumer']
+
+
+from websockets.consumers.service_offer import ServiceOfferConsumer
+from websockets.consumers.service_unavailabilities import ServiceUnavailabilitiesConsumer
--- api/websockets/templates.py
+++ api/websockets/templates.py
@@ -1,1 +1,2 @@
 SERVICE_OFFER_GROUP = 'service_offer_{}'
+SERVICE_UNAVAILABILITIES_GROUP = 'service_unavailabilities_{}'

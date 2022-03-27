60b0614758b1c22a03a74f0de093ed87d80c8843
Sebastian Derendarz <Derendarz99@gmail.com>
2022-03-27 10:57:31 +0200

    T-30 Change enum return values in ServiceOfferSerializer from enum.name to enum.value

--- api/barber/serializers.py
+++ api/barber/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers as rest_serializers
 
-from barber import models
+from barber import models, value_objects
 
 
 class ServiceOfferListSerializer(rest_serializers.ModelSerializer):
@@ -10,6 +10,19 @@ class ServiceOfferListSerializer(rest_serializers.ModelSerializer):
 
 
 class ServiceOfferSerializer(rest_serializers.ModelSerializer):
+    open_hours = rest_serializers.SerializerMethodField()
+    specialization = rest_serializers.SerializerMethodField()
+    working_days = rest_serializers.SerializerMethodField()
+
     class Meta:
         model = models.ServiceOffer
         exclude = ('author', 'created_at', 'image', 'status', 'updated_at')
+
+    def get_open_hours(self, service_offer: models.ServiceOffer) -> str:
+        return getattr(value_objects.OpenHours, service_offer.open_hours).value
+
+    def get_specialization(self, service_offer: models.ServiceOffer) -> str:
+        return getattr(value_objects.BarberSpecialization, service_offer.specialization).value
+
+    def get_working_days(self, service_offer: models.ServiceOffer) -> str:
+        return getattr(value_objects.WorkingDays, service_offer.working_days).value

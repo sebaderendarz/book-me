edb13d15407ee3c404bb15ff72dea85654b0fb4f
Sebastian Derendarz <Derendarz99@gmail.com>
2022-03-27 12:06:23 +0200

    T-30 Add service unavailabilities fetching and serialization for websocket consumer

--- api/websockets/utils.py
+++ api/websockets/utils.py
@@ -2,22 +2,42 @@ from datetime import datetime
 
 from django.db.models import query
 
-from customer import models, value_objects
+from barber import models as barber_models
+from customer import models as customer_models, value_objects
 
 
 def get_active_service_orders(service_offer_id: int) -> query.QuerySet:
     return (
-        models.ServiceOrder.objects.filter(
+        customer_models.ServiceOrder.objects.filter(
             offer_id=service_offer_id, service_time__gt=datetime.now()
         )
         .exclude(status=value_objects.OrderStatus.CLOSED.name)
+        .values('service_time')
         .order_by('service_time')
         .all()
     )
 
 
 def serialize_service_orders(service_orders: query.QuerySet) -> list:
-    serialized_orders = []
-    for service_order in service_orders:
-        serialized_orders.append(service_order.service_time.isoformat())
-    return serialized_orders
+    return [service_order['service_time'].isoformat() for service_order in service_orders]
+
+
+def get_future_service_unavailabilities(service_offer_id: int) -> query.QuerySet:
+    today = datetime.now().date()
+    return (
+        barber_models.ServiceUnavailability.objects.filter(
+            service_offer_id=service_offer_id, start_date__gte=today
+        )
+        .values('start_date', 'end_date')
+        .order_by('start_date')
+        .all()
+    )
+
+
+def serialize_service_unavailabilities(service_unavailabilities: query.QuerySet) -> list:
+    serialized_unavailabilities = []
+    for service_unavailability in service_unavailabilities:
+        serialized_unavailabilities.append(
+            {key: service_unavailability[key].isoformat() for key in service_unavailability.keys()}
+        )
+    return serialized_unavailabilities

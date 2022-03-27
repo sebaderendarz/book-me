1ec0c72d33ad8bf5762d3fec5f6003be778b81b2
Sebastian Derendarz <Derendarz99@gmail.com>
2022-03-27 17:47:29 +0200

    T-30 Add connection with websockets to service offer page

--- frontend/src/pages/BarberOfferPage.js
+++ frontend/src/pages/BarberOfferPage.js
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import BarberOfferDescription from "../components/BarberOfferDescription";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import BarberOfferDescription from "../components/BarberOfferDescription";
 import useAxios from "../utils/useAxios";
 
 // Ideas how to implement baber ordering:
@@ -35,18 +35,33 @@ import useAxios from "../utils/useAxios";
 //     "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
 // };
 
+const { REACT_APP_WS_BASE_URL } = process.env;
+const WEBSOCKET_API_URL = `${REACT_APP_WS_BASE_URL}websockets`;
+
 const theme = createTheme();
 
 export default function BarberOfferPage() {
   const { offer_id } = useParams();
+  const [absences, setAbsences] = useState([]);
   const [offerDetails, setOfferDetails] = useState(null);
+  const [orders, setOrders] = useState([]);
   const api = useAxios();
   const navigate = useNavigate();
+  const absenceWs = useRef(null);
+  const ordersWs = useRef(null);
 
   useEffect(() => {
     validateOfferIdParam();
     getServiceOfferDetails();
-    // connect to websocket
+    connectToAbsenceWebsocket();
+    connectToOrdersWebsocket();
+
+    const absenceWsCurrent = absenceWs.current;
+    const ordersWsCurrent = ordersWs.current;
+    return () => {
+      absenceWsCurrent.close();
+      ordersWsCurrent.close();
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -72,6 +87,26 @@ export default function BarberOfferPage() {
       .catch(() => {
         navigate("/not_found");
       });
+  };
+
+  const connectToAbsenceWebsocket = () => {
+    absenceWs.current = new WebSocket(
+      `${WEBSOCKET_API_URL}/service_unavailabilities/${offer_id}/`
+    );
+    absenceWs.current.onmessage = (e) => {
+      const message = JSON.parse(e.data);
+      setAbsences(message ? message : []);
+    };
+  };
+
+  const connectToOrdersWebsocket = () => {
+    ordersWs.current = new WebSocket(
+      `${WEBSOCKET_API_URL}/service_orders/${offer_id}/`
+    );
+    ordersWs.current.onmessage = (e) => {
+      const message = JSON.parse(e.data);
+      setOrders(message ? message : []);
+    };
   };
 
   return (
